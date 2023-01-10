221e5f49f4accca68caf45f0396755cdff849e23
Yuzhen Mao <57878927+yuzhenmao@users.noreply.github.com>
2023-01-09 17:13:08 -0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,269 +1,150 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+ 
+@yuzhenmao 
+yuzhenmao
+/
+Skyformer
+Public
+forked from pkuzengqi/Skyformer
+Fork your own copy of yuzhenmao/Skyformer
+Code
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+Skyformer
+/
+src
+/
+config.py
+in
+main
+ 
 
+Spaces
 
+2
+
+No wrap
+1
+​
+2
+​
+3
 # seq_len=4096, nb_feat=256
+4
 # seq_len=512, nb_feat=64
+5
 ### big bird: n*nbf = (r+w+g)*b*n, block_size = nb_feat / 10
-
+6
+​
+7
 listops = {
+8
               "dataset":{
+9
                   "train":96000,
+10
                   "dev":2000,
+11
                   "test":2000,
+12
               },
+13
               "model":{
+14
                   "learn_pos_emb":True,
+15
                   "tied_weights":False,
+16
                   "embedding_dim":512, 
+17
                   "transformer_dim":512, 
+18
                   "transformer_hidden_dim":1024, 
+19
                   "head_dim":512, 
+20
                   "num_head":2, 
+21
                   "num_layers":2,
+22
                   "vocab_size":32,
+23
                   "max_seq_len":8000,
+24
                   "dropout_prob":0.1,
+25
                   "attention_dropout":0.1,
+26
                   "pooling_mode":"MEAN",
+27
                   "num_classes":10,
+28
               },
+29
               "training":{
+30
                   "batch_size":8, 
+31
                   "learning_rate":0.0001,
+32
                   "warmup":1000,
+33
                   "lr_decay":"linear",
+34
                   "weight_decay":0,
+35
                   "eval_frequency":500, 
+36
                   "num_train_steps":50000,
+37
                   "num_init_steps":1000,
+38
                   "num_eval_steps":62,
+39
                   "patience":10, 
+40
               },
+41
               "extra_attn_config":{
+42
                   "softmax":{"bz_rate":1,},
+43
                   "softmaxRBF32":{"bz_rate":1},
+44
                   "kernelized":{"bz_rate":1},
-
-                  "sketchedRBF32128":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
-                  "skyformer":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
-
-                  "nystrom":{"bz_rate":1,"num_landmarks":128},
-                  "linformer":{"bz_rate":1,"linformer_k":128},
-                  "performer":{"bz_rate":1,"rp_dim":128, "kernel_type":"exp"}, 
-                  "informer":{"bz_rate":1,"in_nb_features":128},
-                  "reformer":{"bz_rate":1,"num_hash":2},
-                  "bigbird":{"bz_rate":1,"num_random_blocks":3, "block_size":64},
-
-                  }
-          }
-pathfinder = {
-           "model":{
-               "learn_pos_emb":True,
-               "tied_weights":False,
-               "embedding_dim":256, 
-               "transformer_dim":256, 
-               "transformer_hidden_dim":512, 
-               "head_dim":256,
-               "num_head":2, 
-               "num_layers":2,
-               "vocab_size":512,
-               "max_seq_len":1024,
-               "dropout_prob":0.1,
-               "attention_dropout":0.1,
-               "pooling_mode":"MEAN",
-               "num_classes": 2,
-           },
-           "training":{
-               "batch_size":16, 
-               "learning_rate":0.0002,
-               "warmup":312,
-               "lr_decay":"linear",
-               "weight_decay":0,
-               "eval_frequency":500, 
-               "num_train_steps":50000, 
-               "num_init_steps":3500,
-               "num_eval_steps":156, 
-               "patience":10, 
-           },
-           "extra_attn_config":{
-               "softmax":{"bz_rate":1,},
-               "softmaxRBF32":{"bz_rate":1},
-               "kernelized":{"bz_rate":1},
-
-               "sketchedRBF32128":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
-               "skyformer":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
-
-               "nystrom":{"bz_rate":1,"num_landmarks":128},
-               "linformer":{"bz_rate":1,"linformer_k":128},
-               "performer":{"bz_rate":1,"rp_dim":128, "kernel_type":"exp"}, #rp_dim = nb_features
-               "informer":{"bz_rate":2,"in_nb_features":128},
-               "bigbird":{"bz_rate":1,"num_random_blocks":3, "block_size":64},
-               "reformer":{"bz_rate":1,"num_hash":2},
-
-           }
-       }
-retrieval={
-              "dataset":{
-                  "train":147086,
-                  "dev":18090,
-                  "test":17437,
-              },
-              "model":{
-                  "learn_pos_emb":True,
-                  "tied_weights":False,
-                  "embedding_dim":256, 
-                  "transformer_dim":256, 
-                  "transformer_hidden_dim":512, 
-                  "head_dim":256, 
-                  "num_head":2, 
-                  "num_layers":2,
-                  "vocab_size":512,
-                  "max_seq_len":4000,
-                  "dropout_prob":0.1,
-                  "attention_dropout":0.1,
-                  "pooling_mode":"MEAN",
-                  "num_classes": 2,
-              },
-              "training":{
-                  "batch_size":16, 
-                  "learning_rate":0.0002,
-                  "warmup":800,
-                  "lr_decay":"linear",
-                  "weight_decay":0,
-                  "eval_frequency":1000,
-                  "num_train_steps":50000, 
-                  "num_init_steps":3000,
-                  "num_eval_steps":300, 
-                  "patience":10, 
-              },
-              "extra_attn_config":{
-                  "softmax":{"bz_rate":1,},
-                  "softmaxRBF32":{"bz_rate":2},
-                  "kernelized":{"bz_rate":2},
-
-                  "sketchedRBF32128":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
-                  "skyformer":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
-                  
-
-                  "nystrom":{"bz_rate":1,"num_landmarks":128},
-                  "linformer":{"bz_rate":1,"linformer_k":128},
-                  "performer":{"bz_rate":1,"rp_dim":128, "kernel_type":"exp"}, #rp_dim = nb_features
-                  "informer":{"bz_rate":1,"in_nb_features":128},
-                  "bigbird":{"bz_rate":1,"num_random_blocks":3, "block_size":64},
-                  "reformer":{"bz_rate":1,"num_hash":2},
-              }
-          }
-text={
-         "dataset":{
-             "train":25000,
-             "dev":25000,
-             "test":25000,
-         },
-         "model":{
-             "learn_pos_emb":True,
-             "tied_weights":False,
-             "embedding_dim":512, 
-             "transformer_dim":512, 
-             "transformer_hidden_dim":1024, 
-             "head_dim":512, 
-             "num_head":2, 
-             "num_layers":2,
-             "vocab_size":512,
-             "max_seq_len":4000, 
-             "dropout_prob":0.1,
-             "attention_dropout":0.1,
-             "pooling_mode":"MEAN",
-             "num_classes": 2,
-         },
-         "training":{
-             "batch_size":16,
-             "learning_rate":0.0001,
-             "warmup":80, 
-             "lr_decay":"linear",
-             "weight_decay":0,
-             "eval_frequency":500, 
-             "num_train_steps":50000,
-             "num_init_steps":3000,
-             "num_eval_steps":200, 
-             "patience":10, 
-         },
-         "extra_attn_config":{
-             "softmax":{"bz_rate":1},
-             "softmaxRBF32":{"bz_rate":2},
-             "kernelized":{"bz_rate":2},
-
-             "sketchedRBF32128":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
-             "skyformer":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
-             
-             "nystrom":{"bz_rate":1,"num_landmarks":128},
-             "linformer":{"bz_rate":1,"linformer_k":128},
-             "performer":{"bz_rate":1,"rp_dim":128, "kernel_type":"exp"}, #rp_dim = nb_features
-             "informer":{"bz_rate":1,"in_nb_features":128},
-             "bigbird":{"bz_rate":1,"num_random_blocks":3, "block_size":64},
-             "reformer":{"bz_rate":1,"num_hash":2},
-         }
-     }
-
-
-image={
-        "dataset":{
-            "train":45000,
-            "dev":5000,
-            "test":10000,
-        },
-        "model":{
-            "learn_pos_emb":True,
-            "tied_weights":False,
-            "embedding_dim":256,
-            "transformer_dim":256,
-            "transformer_hidden_dim":512,
-            "head_dim":256,
-            "num_head":2,
-            "num_layers":2,
-            "vocab_size":256, 
-            "max_seq_len":1024,
-            "dropout_prob":0.1, 
-            "attention_dropout":0.1,
-            "pooling_mode":"MEAN",
-            "num_classes": 10,
-        },
-        "training":{
-            "batch_size":16, 
-            "learning_rate":0.0001, 
-            "warmup":175,
-            "lr_decay":"linear",
-            "weight_decay":0,
-            "eval_frequency":50,  
-            "num_train_steps":50000, 
-            "num_init_steps":0,
-            "num_eval_steps":20,
-        },
-
-        "extra_attn_config":{
-            "softmax":{"bz_rate":1},
-
-            "softmaxRBF32":{"bz_rate":1}, # for stability
-            "kernelized":{"bz_rate":1}, # for stability
-
-            "sketchedRBF32128":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
-            "skyformer":{"bz_rate":1, "nb_features":128, "sketched_kernel":"kernel_RS_RBF", "accumulation":1, "sampling_factor":4, "no_projection":False},
-
-
-            "nystrom":{"bz_rate":1,"num_landmarks":128},
-            "linformer":{"bz_rate":1,"linformer_k":128},
-            "performer":{"bz_rate":1,"rp_dim":128, "kernel_type":"exp"}, #rp_dim = nb_features
-            "informer":{"bz_rate":2,"in_nb_features":128},
-            "bigbird":{"bz_rate":1,"num_random_blocks":3, "block_size":64},
-            "reformer":{"bz_rate":1,"num_hash":2},
-        }
-}
-
-Config = {
-    "lra-listops":listops,
-    "lra-pathfinder":pathfinder,
-    "lra-retrieval":retrieval,
-    "lra-text":text,
-    "lra-image":image,
-}
-
-Config["lra-pathfinder32-curv_contour_length_14"] = Config["lra-pathfinder"]
-
+45
+​
+@yuzhenmao
+Commit changes
+Commit summary
+Create config.py
+Optional extended description
+Add an optional extended description…
+ Commit directly to the main branch.
+ Create a new branch for this commit and start a pull request. Learn more about pull requests.
+ 
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
